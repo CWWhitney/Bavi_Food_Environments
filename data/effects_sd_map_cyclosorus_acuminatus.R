40fd87e1ac7ee012b68e9b22e97172b6318c0c0a
CWWhitney <whitney.cory@gmail.com>
2025-01-09 07:47:52 +0700

    data to model bavi forest food

---
+++ data/effects_sd_map_cyclosorus_acuminatus.R
@@ -0,0 +1,39 @@
+# effects_sd_map_cyclosorus_acuminatus
+
+effects_sd_map_cyclosorus_acuminatus <- list(
+  SpeciesConservation = list(
+    control_access = list(Effect = 0.8, SD = 0.05),
+    provide_access_control_harvest = list(Effect = 0.6, SD = 0.05),
+    allow_access_forest_parts = list(Effect = 0.5, SD = 0.05),
+    allow_access_limited_time = list(Effect = 0.4, SD = 0.05),
+    provide_seedlings = list(Effect = 0.3, SD = 0.05)
+  ),
+  DietaryDiversity = list(
+    control_access = list(Effect = -0.5, SD = 0.30),
+    provide_access_control_harvest = list(Effect = 0.2, SD = 0.10),
+    allow_access_forest_parts = list(Effect = 0.3, SD = 0.10),
+    allow_access_limited_time = list(Effect = 0.4, SD = 0.10),
+    provide_seedlings = list(Effect = 0.5, SD = 0.40)
+  ),
+  SustainableFoodEnv = list(
+    control_access = list(Effect = 0.1, SD = 0.08),
+    provide_access_control_harvest = list(Effect = 0.4, SD = 0.08),
+    allow_access_forest_parts = list(Effect = 0.6, SD = 0.08),
+    allow_access_limited_time = list(Effect = 0.7, SD = 0.08),
+    provide_seedlings = list(Effect = 0.9, SD = 0.08)
+  ),
+  ParkExpenditure = list(
+    control_access = list(Effect = 0.9, SD = 0.20),
+    provide_access_control_harvest = list(Effect = 0.7, SD = 0.20),
+    allow_access_forest_parts = list(Effect = 0.5, SD = 0.20),
+    allow_access_limited_time = list(Effect = 0.4, SD = 0.20),
+    provide_seedlings = list(Effect = 0.3, SD = 0.20)
+  ),
+  TraditionalKnowledge = list(
+    control_access = list(Effect = 0.2, SD = 0.15),
+    provide_access_control_harvest = list(Effect = 0.5, SD = 0.15),
+    allow_access_forest_parts = list(Effect = 0.6, SD = 0.15),
+    allow_access_limited_time = list(Effect = 0.7, SD = 0.15),
+    provide_seedlings = list(Effect = 0.8, SD = 0.15)
+  )
+)
